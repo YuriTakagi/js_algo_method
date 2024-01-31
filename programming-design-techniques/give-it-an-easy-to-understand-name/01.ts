5c4e25eb9fbd190faeb78e2daea481e3b0945bf8
y-takagiiii <takagiyuri0712@gmail.com>
2024-01-31 10:04:31 +0900

    命名規則

---
+++ programming-design-techniques/give-it-an-easy-to-understand-name/01.ts
@@ -0,0 +1,32 @@
+/*
+次の Python コードには、命名規則 (★) に従っていない名前があります。それはどれか、選択肢から 1 つ選んでください。
+
+class Tortoise:
+    def __init__(self, name, favorite_food):
+        self.name = name
+        self.favorite_food = favorite_food
+    def introduce(self):
+        print(f"{self.name} は{self.favorite_food}が好きです。")
+
+class Tortoise_English(Tortoise):
+    def introduce(self):
+        print(f"{self.name} likes {self.favorite_food}.")
+
+aruru = Tortoise("aruru", "水草")
+aruru.introduce()
+
+iruru = Tortoise_English("iruru", "fish")
+iruru.introduce()
+<選択肢>
+
+1.　クラス名 Tortoise_English
+
+2.　クラスの属性名 favorite_food
+
+3.　クラスのメソッド名 introduce
+
+4.　クラスのインスタンス名 aruru
+
+*/
+
+1;
